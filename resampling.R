b41dc283a5c3a9b2286a80001d7893801f34c124
ntabgoba <ntabgoba@gmail.com>
2016-09-14 20:00:43 +0900

    resample

--- resampling.R
+++ resampling.R
@@ -24,3 +24,20 @@ alpha = function(x,y){
         cxy= cov(x,y)
         (vy-cxy)/(vx+vy-2*cxy)
 }
+alpha(Xy$X1,Xy$y)
+# alpha(Xy$X2, Xy$y)
+alpha.fn = function(data,index){
+        with(data[index,],alpha(Xy$X1,Xy$y))
+}
+
+alpha.fn<-function(data, index) {
+        fit1<-lm(y~., data=Xy[index,])
+        coefficients(fit1)[['X1']]
+}
+set.seed(1)
+alpha.fn (Xy,sample(1:100,100,replace=TRUE))
+
+boot(Xy,function(tptp,idx){
+        idx <- rep(sample(0:9,10,replace=T) * 100,each= 100) + 1:100
+        coef(glm(y~X1+X2,data=tptp[idx,]))[[2]]
+     },R=10000)
